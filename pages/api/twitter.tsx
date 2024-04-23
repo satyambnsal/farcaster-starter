6c7e6c9f2aa99087e12c655b60f6200f4f1af19d
Satyam Bansal <satyamsgsits1994@gmail.com>
2024-04-23 21:57:24 +0530

    Add twitter cards

---
+++ pages/api/twitter.tsx
@@ -0,0 +1,32 @@
+import type { NextApiRequest, NextApiResponse } from 'next'
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+  try {
+    const score = req.query['score']
+    if (!score) {
+      return res.status(400).send('Missing Score')
+    }
+
+    const imageUrl = `${process.env['HOST']}/api/image-v2?score=${score}`
+
+    res.setHeader('Content-Type', 'text/html')
+    res.status(200).send(`
+      <!DOCTYPE html>
+      <html>
+        <head>
+          <title>RYO Progress</title>
+          <meta property="og:title" content="RYO Progress">
+          <meta property="og:image" content="${imageUrl}">
+          <meta property="og:description" content="Check out my progress!" />
+          <meta property="og:url" content="https://farcaster-starter.vercel.app/" />
+          <meta name="twitter:card" content="summary_large_image" />
+        </head>
+        <body>
+        </body>
+      </html>
+    `)
+  } catch (error) {
+    console.error(error)
+    res.status(500).send('Error generating image')
+  }
+}
